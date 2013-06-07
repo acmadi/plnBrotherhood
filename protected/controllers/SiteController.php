084a5f486cc4ac8282791ab9f6916f6b0864bb4d
Irvan Aditya <iaditya9391@gmail.com>
2013-06-07 14:14:42 +0700

    test interface awal

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -21,15 +21,22 @@ class SiteController extends Controller
 		);
 	}
 
+	public $defaultAction = 'dashboard';
+
 	/**
 	 * This is the default 'index' action that is invoked
 	 * when an action is not explicitly requested by users.
 	 */
-	public function actionIndex()
+	public function actionDashboard()
 	{
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
-		$this->render('index');
+		if (Yii::app()->user->isGuest) {
+			$this->redirect(array('site/login'));
+		}
+		else {
+			$this->render('dashboard');
+		}
 	}
 
 	/**
@@ -49,28 +56,28 @@ class SiteController extends Controller
 	/**
 	 * Displays the contact page
 	 */
-	public function actionContact()
-	{
-		$model=new ContactForm;
-		if(isset($_POST['ContactForm']))
-		{
-			$model->attributes=$_POST['ContactForm'];
-			if($model->validate())
-			{
-				$name='=?UTF-8?B?'.base64_encode($model->name).'?=';
-				$subject='=?UTF-8?B?'.base64_encode($model->subject).'?=';
-				$headers="From: $name <{$model->email}>\r\n".
-					"Reply-To: {$model->email}\r\n".
-					"MIME-Version: 1.0\r\n".
-					"Content-type: text/plain; charset=UTF-8";
+	// public function actionContact()
+	// {
+	// 	$model=new ContactForm;
+	// 	if(isset($_POST['ContactForm']))
+	// 	{
+	// 		$model->attributes=$_POST['ContactForm'];
+	// 		if($model->validate())
+	// 		{
+	// 			$name='=?UTF-8?B?'.base64_encode($model->name).'?=';
+	// 			$subject='=?UTF-8?B?'.base64_encode($model->subject).'?=';
+	// 			$headers="From: $name <{$model->email}>\r\n".
+	// 				"Reply-To: {$model->email}\r\n".
+	// 				"MIME-Version: 1.0\r\n".
+	// 				"Content-type: text/plain; charset=UTF-8";
 
-				mail(Yii::app()->params['adminEmail'],$subject,$model->body,$headers);
-				Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
-				$this->refresh();
-			}
-		}
-		$this->render('contact',array('model'=>$model));
-	}
+	// 			mail(Yii::app()->params['adminEmail'],$subject,$model->body,$headers);
+	// 			Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
+	// 			$this->refresh();
+	// 		}
+	// 	}
+	// 	$this->render('contact',array('model'=>$model));
+	// }
 
 	/**
 	 * Displays the login page
