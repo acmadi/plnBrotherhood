8e30c11ed9405571079785101841748cb82e4ef7
kevin_20592 <kevin_indra@rocketmail.com>
2013-06-14 14:08:27 +0700

    pull doank

--- protected/controllers/DocxController.php
+++ protected/controllers/DocxController.php
@@ -18,7 +18,7 @@ class DocxController extends Controller
 	
 	public function actionDownload()
 	{
-		$this->doccy->newFile('t1.docx'); // template.docx must be located in protected/views/report/template.docx where "report" is the name of the controller from which is renderDocx called (alternatively you must configure option "templatePath")
+		$this->doccy->newFile('notadinas.docx'); // template.docx must be located in protected/views/report/template.docx where "report" is the name of the controller from which is renderDocx called (alternatively you must configure option "templatePath")
 		$this->doccy->phpdocx->assignToHeader("#HEADER1#","Test1"); // basic field mapping to header
 		$this->doccy->phpdocx->assignToFooter("#FOOTER1#","Test2"); // basic field mapping to footer
 		$this->doccy->phpdocx->assign("#TITLE1#","Pet shop BOYS list"); // basic field mapping
@@ -27,9 +27,22 @@ class DocxController extends Controller
 		//$this->doccy->phpdocx->assignNestedBlock("pets",array(array("#PETNAME#"=>"Rox")),array("members"=>2)); // would create a block pets for jane doe with the name rox
 		// $this->doccy->phpdocx->assignNestedBlock("toys",array(array("#TOYNAME#"=>"Ball"),array("#TOYNAME#"=>"Frisbee"),array("#TOYNAME#"=>"Box")),array("members"=>1,"pets"=>1)); // would create a block toy for rex
 		// $this->doccy->phpdocx->assignNestedBlock("toys",array(array("#TOYNAME#"=>"Frisbee")),array("members"=>2,"pets"=>1)); // would create a block toy for rox
-		$this->doccy->phpdocx->assign('#NOMOR#', 'a');
-		$this->doccy->phpdocx->assign('#KEPADA#', 'b');
-		$this->doccy->phpdocx->assign('#DARI#', 'c');
+		$this->doccy->phpdocx->assign('#nosurat#', 'a');
+		$this->doccy->phpdocx->assign('#tahunsurat#', 'b');
+		$this->doccy->phpdocx->assign('#kepada#', 'c');
+		$this->doccy->phpdocx->assign('#dari#', 'd');
+		$this->doccy->phpdocx->assign('#lampiran#', 'e');
+		$this->doccy->phpdocx->assign('#tanggal#', 'f');
+		$this->doccy->phpdocx->assign('#perihal#', 'g');
+		$this->doccy->phpdocx->assign('#anggaran#', 'h');
+		$this->doccy->phpdocx->assign('#sumber#', 'i');
+		$this->doccy->phpdocx->assign('#torrks#', 'j');
+		$this->doccy->phpdocx->assign('#rab#', 'k');
+		$this->doccy->phpdocx->assign('#metode#', 'l');
+		$this->doccy->phpdocx->assign('#targetspk#', 'm');
+		$this->doccy->phpdocx->assign('#user#', 'n');
+		$this->doccy->phpdocx->assign('#nonotadinas#', 'o');
+		$this->doccy->phpdocx->assign('#namapengadaan#', 'p');
 		$this->renderDocx("test.docx", false); // use $forceDownload=false in order to (just) store file in the outputPath folder.
 		$this->render('download');
 	}
