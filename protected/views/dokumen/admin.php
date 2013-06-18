e8c0b72051a718b79d2f438a760c20c0ef341fe2
Gilang Laksana Laba <gilang.laba@gmail.com>
2013-06-18 10:10:18 +0700

    pull

--- protected/views/dokumen/admin.php
+++ protected/views/dokumen/admin.php
@@ -2,10 +2,10 @@
 /* @var $this DokumenController */
 /* @var $model Dokumen */
 
-// $this->breadcrumbs=array(
-	// 'Dokumens'=>array('index'),
-	// 'Manage',
-// );
+$this->breadcrumbs=array(
+	'Dokumens'=>array('index'),
+	'Manage',
+);
 
 $this->menu=array(
 	array('label'=>'List Dokumen', 'url'=>array('index')),
@@ -51,11 +51,6 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		'tempat',
 		'id_pengadaan',
 		'status_upload',
-		/*
-		'waktu_upload',
-		'pengunggah',
-		'link_penyimpanan',
-		*/
 		array(
 			'class'=>'CButtonColumn',
 		),
