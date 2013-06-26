971b7debf06faaba88ff40dfac3f85bf9f631ad1
Irvan Aditya <iaditya9391@gmail.com>
2013-06-26 11:35:07 +0700

    beresin chart pengadaan per divisi

--- protected/views/site/statistik.php
+++ protected/views/site/statistik.php
@@ -7,13 +7,25 @@ $this->pageTitle=Yii::app()->name . ' | Statistik Pengadaan';
 <div id="sidebar">
 	<?php $this->beginWidget('zii.widgets.CPortlet'); ?>
 	<ul>
-		<li><?php echo CHtml::link('Pengadaan per divisi', array('site/statistik')) ?></li>
-		<li><?php echo CHtml::link('Status pengadaan', array('site/statistik')) ?></li>
+		<li><?php echo CHtml::link('Pengadaan per divisi', array('site/statistik', 'chart'=>'1')) ?></li>
 	</ul>
 	<?php $this->endWidget(); ?>
 </div>
 
 <div id="maincontent">
+	<div id="menuform">
+		<?php
+		    $this->widget('zii.widgets.CMenu', array(
+		        'items'=>array(
+		                array('label'=>'Total', 'url'=>array('site/statistik', 'chart'=>'1')),
+		                array('label'=>'Berlangsung', 'url'=>array('site/statistik', 'chart'=>'2')),
+		                array('label'=>'Selesai', 'url'=>array('site/statistik', 'chart'=>'3')),
+		                array('label'=>'Gagal', 'url'=>array('site/statistik', 'chart'=>'4')),
+		        ),
+		    ));
+		?>
+	</div>
+	<br />
 	<?php $this->widget('HighchartsWidget', array(
 			'options'=>array(
 				'tooltip'=>array(
