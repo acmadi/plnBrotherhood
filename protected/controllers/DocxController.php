618276c3c75e936674ec1ed588c279cf7665b2af
13510103 <13510103@std.stei.itb.ac.id>
2013-07-09 19:05:52 +0700

    aaa

--- protected/controllers/DocxController.php
+++ protected/controllers/DocxController.php
@@ -2617,7 +2617,7 @@ class DocxController extends Controller
 	}
 	
 	function getPenyediaLulusKesimpulan($idpeng,$tahap){
-		$arraypenyedia = PenerimaPengadaan::model()->findAll($tahap . ' = "1"  and id_pengadaan = ' . $idpeng);
+		$arraypenyedia = PenerimaPengadaan::model()->findAll($tahap . ' = "1"  and id_pengadaan = ' . $idpeng . ' order by nilai');
 		$stringpenyedia = "";
 				
 		if($arraypenyedia == null){
@@ -2629,7 +2629,7 @@ class DocxController extends Controller
 									'Alamat			 	: ' . $arraypenyedia[$i]->alamat . '<w:br/>' . 
 									'NPWP			 	: ' . $arraypenyedia[$i]->npwp . '<w:br/>' .
 									'Nilai Penawaran 	: ' . RupiahMaker::convertInt($arraypenyedia[$i]->nilai) . '<w:br/>' .
-									'Terbilang 			: ' . RupiahMaker::TerbilangMaker($arraypenyedia[$i]->nilai) . ', sudah termasuk pajak sesuai dengan ketentuan yang berlaku. <w:br/>'
+									'Terbilang 			: ' . RupiahMaker::TerbilangMaker($arraypenyedia[$i]->nilai) . ', sudah termasuk pajak sesuai dengan ketentuan yang berlaku. <w:br/> <w:br/>'
 									;
 			}
 		}		
@@ -2658,8 +2658,6 @@ class DocxController extends Controller
 	function persenMaker($nilai,$hps){
 		return 0;
 	}
-	
-	
 	
 }
 ?>
