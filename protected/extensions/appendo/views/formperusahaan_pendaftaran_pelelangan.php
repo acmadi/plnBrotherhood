8c5d630deec30063b7bde2384052ba75869d2abc
13510103 <13510103@std.stei.itb.ac.id>
2013-07-10 19:02:01 +0700

    pendaftaran pelelangan pc

--- protected/extensions/appendo/views/formperusahaan_pendaftaran_pelelangan.php
+++ protected/extensions/appendo/views/formperusahaan_pendaftaran_pelelangan.php
@@ -10,7 +10,7 @@
 		<tr>
 			<td><?php echo CHtml::textField('perusahaan[]','',array('style'=>'width:120px')); ?></td>            
             <td>
-				<?php echo CHtml::dropDownList('undangan_pengambilan_dokumen[]',"string",
+				<?php echo CHtml::dropDownList('pendaftaran_pelelangan_pq[]',"string",
 					array(
 						"1"=>"Lulus",
 						"0"=>"Tidak Lulus",
@@ -24,7 +24,7 @@
 			<tr>
 				<td><?php echo CHtml::textField('perusahaan[]',$model[$i]->perusahaan,array('style'=>'width:120px')); ?></td>				
 				<td>
-					<?php echo CHtml::dropDownList('undangan_pengambilan_dokumen[]',$model[$i]->status,
+					<?php echo CHtml::dropDownList('pendaftaran_pelelangan_pq[]',$model[$i]->pendaftaran_pelelangan_pq,
 						array(
 							"1"=>"Lulus",
 							"0"=>"Tidak Lulus",
