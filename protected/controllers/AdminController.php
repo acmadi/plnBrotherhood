37879860c80336a6d72f2f4c3f1cc6b9f6d823cc
Irvan Aditya <iaditya9391@gmail.com>
2013-07-11 01:57:33 +0700

    bikin page admin

--- protected/controllers/AdminController.php
+++ protected/controllers/AdminController.php
@@ -4,57 +4,76 @@ class AdminController extends Controller
 {
 	public function actionPengguna()
 	{
-		$model = new Anggota('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['Anggota'])){
-			$model->attributes = $_GET['Anggota'];
+		if (Yii::app()->user->getState('role') == 'admin') {
+			$model = new Anggota('search');
+			$model->unsetAttributes();  // clear any default values
+			if(isset($_GET['Anggota'])){
+				$model->attributes = $_GET['Anggota'];
+			}
+			$this->render('pengguna', array(
+				'model'=>$model,
+			));
 		}
-		$this->render('pengguna', array(
-			'model'=>$model,
-		));
 	}
 
 	public function actionPanitia()
 	{
-		$model = new Panitia('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['Panitia'])){
-			$model->attributes = $_GET['Panitia'];
+		if (Yii::app()->user->getState('role') == 'admin') {
+			$model = new Panitia('search');
+			$model->unsetAttributes();  // clear any default values
+			if(isset($_GET['Panitia'])){
+				$model->attributes = $_GET['Panitia'];
+			}
+			$this->render('panitia', array(
+				'model'=>$model,
+			));
 		}
-		$this->render('panitia', array(
-			'model'=>$model,
-		));
 	}
 
 	public function actionKdiv()
 	{
-		$model = new Kdivmum('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['Kdivmum'])){
-			$model->attributes = $_GET['Kdivmum'];
+		if (Yii::app()->user->getState('role') == 'admin') {
+			$model = new Kdivmum('search');
+			$model->unsetAttributes();  // clear any default values
+			if(isset($_GET['Kdivmum'])){
+				$model->attributes = $_GET['Kdivmum'];
+			}
+			$this->render('kdiv', array(
+				'model'=>$model,
+			));
 		}
-		$this->render('kdiv', array(
-			'model'=>$model,
-		));
 	}
 
 	public function actionDivisi()
 	{
-		$model = new Divisi('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['Divisi'])){
-			$model->attributes = $_GET['Divisi'];
+		if (Yii::app()->user->getState('role') == 'admin') {
+			$model = new Divisi('search');
+			$model->unsetAttributes();  // clear any default values
+			if(isset($_GET['Divisi'])){
+				$model->attributes = $_GET['Divisi'];
+			}
+			$this->render('divisi', array(
+				'model'=>$model,
+			));
 		}
-		$this->render('divisi', array(
-			'model'=>$model,
-		));
 	}
 
 	public function actionAdmin()
 	{
-		$admin = Admin::model()->findAll()[0];
-		$this->render('admin', array(
-			'admin'=>$admin,
-		));
+		if (Yii::app()->user->getState('role') == 'admin') {
+			$query = Admin::model()->findAll();
+			$admin = $query[0];
+			if (isset($_POST['Admin'])) {
+				$admin->attributes = $_POST['Admin'];
+				if ($admin->validate()) {
+					if ($admin->save(false)) {
+						Yii::app()->user->setFlash('sukses','Data Telah Disimpan');
+					}
+				}
+			}
+			$this->render('admin', array(
+				'admin'=>$admin,
+			));
+		}
 	}
 }
