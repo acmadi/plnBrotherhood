0f914968d146313af2f769bfd77ed453ca669c0c
Aidil Syaputra <dil_syaputra@yahoo.com>
2013-07-14 17:51:10 +0700

    benerin dokumen pasca kualifikasi pemilihan langsung hingga undangan aanwijzing kecuali rks

--- protected/views/site/aanwijzing.php
+++ protected/views/site/aanwijzing.php
@@ -136,7 +136,7 @@ $this->pageTitle=Yii::app()->name . ' | '.$Pengadaan->nama_pengadaan;
 			<ul class="generatedoc">
 				
 				<li><?php echo CHtml::link('Lampiran Berita Acara Aanwijzing', array('docx/download','id'=>$Dokumen2->id_dokumen)); ?></li>
-				<li><?php echo CHtml::link('Daftar Hadir Aanwijzing', array('docx/download','id'=>$DH->id_dokumen)); ?></li>
+				<li><?php echo CHtml::link('Daftar Hadir Aanwijzing', array('xlsx/download','id'=>$DH->id_dokumen)); ?></li>
 			</ul>
 		</div>
 	
