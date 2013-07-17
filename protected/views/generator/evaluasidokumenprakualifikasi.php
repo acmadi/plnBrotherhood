373fad3685afe902d513fea00544d1670c9c3ce2
Hanif Eridaputra <he.23292@gmail.com>
2013-07-17 13:41:38 +0700

    BA Eval Prakualifikasi udah bisa download brur

--- protected/views/generator/evaluasidokumenprakualifikasi.php
+++ protected/views/generator/evaluasidokumenprakualifikasi.php
@@ -103,7 +103,7 @@ $this->pageTitle=Yii::app()->name . ' | '.$Pengadaan->nama_pengadaan;
 				<br/>
 					<h4><b> Daftar Dokumen </b></h4>
 					<ul class="generatedoc">
-						<li><?php echo CHtml::link('Nota Dinas Usulan Hasil Prakualifikasi', array('xlsx/download','id'=>$BAEPK->id_dokumen)); ?></li>
+						<li><?php echo CHtml::link('Berita Acara Evaluasi Dokumen Prakualifikasi', array('xlsx/download','id'=>$BAEPK->id_dokumen)); ?></li>
 					</ul>
 				</div>
 			<?php } ?>
