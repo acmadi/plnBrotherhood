64a059c0a64db94eb7b04c423dbd3f797a191429
Aidil Syaputra <dil_syaputra@yahoo.com>
2013-07-18 00:06:01 +0700

    kontrol anggaran

--- protected/controllers/AnggaranController.php
+++ protected/controllers/AnggaranController.php
@@ -7,6 +7,10 @@ class AnggaranController extends Controller
 		if (Yii::app()->user->getState('role') == 'kdivmum') {
 			$anggaran = array();
 			$divisi = Divisi::model()->findAll();
+			$pagutotal=0;
+			$rabtotal=0;
+			$hpstotal=0;
+			$kontraktotal=0;
 			$i=0;
 			foreach ($divisi as $item) {
 				$paguanggaran=0;
@@ -18,12 +22,82 @@ class AnggaranController extends Controller
 					$paguanggaran=$paguanggaran+NotaDinasPerintahPengadaan::model()->findByPk(Dokumen::model()->find('id_pengadaan = '.$pengadaan->id_pengadaan.' and nama_dokumen = "Nota Dinas Perintah Pengadaan"')->id_dokumen)->pagu_anggaran;
 					$rab=$rab+NotaDinasPermintaan::model()->findByPk(Dokumen::model()->find('id_pengadaan = '.$pengadaan->id_pengadaan.' and nama_dokumen = "Nota Dinas Permintaan"')->id_dokumen)->nilai_biaya_rab;
 					$hps=$hps+Hps::model()->findByPk(Dokumen::model()->find('id_pengadaan = '.$pengadaan->id_pengadaan.' and nama_dokumen = "HPS"')->id_dokumen)->nilai_hps;
+					$kontrak=$kontrak+$pengadaan->biaya;
 				}
-				$anggaran[$i]= array('Nama_Divisi'=>$item->nama_divisi,'pagu_anggaran'=>$paguanggaran,'nilai_rab'=>$rab,'nilai_hps'=>$hps);
+				$pagutotal=$pagutotal+$paguanggaran;
+				$rabtotal=$rabtotal+$rab;
+				$hpstotal=$hpstotal+$hps;
+				$kontraktotal=$kontraktotal+$kontrak;
+				if($rab!=0 && $paguanggaran !=0) {
+					$rab=RupiahMaker::convertIntTanpaRp($rab);
+					$paguanggaran=RupiahMaker::convertIntTanpaRp($paguanggaran);
+					if($hps!=0){
+						$hps=RupiahMaker::convertIntTanpaRp($hps);
+						if($kontrak!=0){
+							$kontrak=RupiahMaker::convertIntTanpaRp($kontrak);
+							$penghematan=$paguanggaran-$kontrak;
+							$persenpenghematan=$penghematan*100/$paguanggaran;
+							$penghematan=RupiahMaker::convertIntTanpaRp($penghematan); 
+						} else {
+							$kontrak='-';
+							$penghematan='-';
+							$persenpenghematan='-';
+						}
+					} else {
+						$hps='-';
+						$kontrak='-';
+						$penghematan='-';
+						$persenpenghematan='-';
+					}
+				} else {
+					$rab='-';
+					$paguanggaran='-';
+					$hps='-';
+					$kontrak='-';
+					$penghematan='-';
+					$persenpenghematan='-';
+				}
+				if($rabtotal!=0 && $pagutotal!=0) {
+					$rabtotal=RupiahMaker::convertInt($rabtotal);
+					$pagutotal=RupiahMaker::convertIntTanpaRp($pagutotal);
+					if($hpstotal!=0){
+						$hpstotal=RupiahMaker::convertIntTanpaRp($hpstotal);
+						if($kontraktotal!=0){
+							$kontraktotal=RupiahMaker::convertIntTanpaRp($kontraktotal);
+							$penghematantotal=$pagutotal-$kontraktotal;
+							$persenpenghematantotal=$penghematantotal*100/$pagutotal;
+							$penghematantotal=RupiahMaker::convertIntTanpaRp($penghematantotal); 
+						} else {
+							$kontraktotal='-';
+							$penghematantotal='-';
+							$persenpenghematantotal='-';
+						}
+					} else {
+						$hpstotal='-';
+						$kontraktotal='-';
+						$penghematantotal='-';
+						$persenpenghematantotal='-';
+					}
+				} else {
+					$rabtotal='-';
+					$rabtotal='-';
+					$hpstotal='-';
+					$kontraktotal='-';
+					$persenpenghematantotal='-';
+					$persenpenghematantotal='-';
+				}
+				$anggaran[$i]= array('username'=>$item->username,'nama_divisi'=>$item->nama_divisi,'pagu_anggaran'=>$paguanggaran,'nilai_rab'=>$rab,'nilai_hps'=>$hps,'nilai_kontrak'=>$kontrak,'penghematan'=>$penghematan,'persen'=>$persenpenghematan);
 				$i++;
 			}
-			$dataanggaran = new CArrayDataProvider($anggaran, array('keyField'=>'Nama_Divisi'));
-			$this->render('kontrolanggaran', array('dataanggaran'=>$dataanggaran,
+			$dataanggaran = new CArrayDataProvider($anggaran,array(
+				'sort'=>array(
+					'attributes'=>array(
+						'username','nama_divisi','pagu_anggaran','nilai_rab','nilai_hps','nilai_kontrak','penghematan','persen',
+					)
+				),
+				'keyField'=>'username',
+			));
+			$this->render('kontrolanggaran', array('dataanggaran'=>$dataanggaran,'pagutotal'=>$pagutotal,'rabtotal'=>$rabtotal,'hpstotal'=>$hpstotal,'kontraktotal'=>$kontraktotal,'penghematantotal'=>$penghematantotal,'persenpenghematantotal'=>$persenpenghematantotal,
 			));
 		}
 	}
@@ -32,12 +106,55 @@ class AnggaranController extends Controller
 	{
 		$id = Yii::app()->getRequest()->getQuery('id');
 		if (Yii::app()->user->getState('role') == 'kdivmum') {
-			$paguanggaran=0;
-			$rab=0;
-			$hps=0;
-			$kontrak=0;
-			$semuapengadaan= Pengadaan::model()->findAll('divisi_pemintan = '.$item->username);
-			$this->render('kontrolanggaran', array(
+			$anggaran = array();
+			$pengadaan= Pengadaan::model()->findAll('divisi_pemintan = '.$id);
+			$i=0;
+			foreach($pengadaan as $item) {
+				$paguanggaran=NotaDinasPerintahPengadaan::model()->findByPk(Dokumen::model()->find('id_pengadaan = '.$pengadaan->id_pengadaan.' and nama_dokumen = "Nota Dinas Perintah Pengadaan"')->id_dokumen)->pagu_anggaran;
+				$rab=NotaDinasPermintaan::model()->findByPk(Dokumen::model()->find('id_pengadaan = '.$pengadaan->id_pengadaan.' and nama_dokumen = "Nota Dinas Permintaan"')->id_dokumen)->nilai_biaya_rab;
+				$hps=Hps::model()->findByPk(Dokumen::model()->find('id_pengadaan = '.$pengadaan->id_pengadaan.' and nama_dokumen = "HPS"')->id_dokumen)->nilai_hps;
+				$kontrak=$pengadaan->biaya;
+				if($rab!=0 && $paguanggaran !=0) {
+					$rab=RupiahMaker::convertIntTanpaRp($rab);
+					$paguanggaran=RupiahMaker::convertIntTanpaRp($paguanggaran);
+					if($hps!=0){
+						$hps=RupiahMaker::convertIntTanpaRp($hps);
+						if($kontrak!=0){
+							$kontrak=RupiahMaker::convertIntTanpaRp($kontrak);
+							$penghematan=$paguanggaran-$kontrak;
+							$persenpenghematan=$penghematan*100/$paguanggaran;
+							$penghematan=RupiahMaker::convertIntTanpaRp($penghematan); 
+						} else {
+							$kontrak='-';
+							$penghematan='-';
+							$persenpenghematan='-';
+						}
+					} else {
+						$hps='-';
+						$kontrak='-';
+						$penghematan='-';
+						$persenpenghematan='-';
+					}
+				} else {
+					$rab='-';
+					$paguanggaran='-';
+					$hps='-';
+					$kontrak='-';
+					$penghematan='-';
+					$persenpenghematan='-';
+				}
+				$anggaran[$i]= array('id_pengadaan'=>$item->id_pengadaan,'nama_pengadaan'=>$item->nama_pengadaan,'pagu_anggaran'=>$paguanggaran,'nilai_rab'=>$rab,'nilai_hps'=>$hps,'nilai_kontrak'=>$kontrak,'penghematan'=>$penghematan,'persen'=>$persenpenghematan);
+				$i++;
+			}
+			$dataanggaran = new CArrayDataProvider($anggaran,array(
+				'sort'=>array(
+					'attributes'=>array(
+						'id_pengadaan','nama_pengadaan','pagu_anggaran','nilai_rab','nilai_hps','nilai_kontrak','penghematan','persen',
+					)
+				),
+				'keyField'=>'id_pengadaan',
+			));
+			$this->render('kontrolanggarandivisi', array(
 			));
 		}
 	}
--- protected/views/Anggaran/kontrolanggaran.php
+++ protected/views/Anggaran/kontrolanggaran.php
@@ -7,57 +7,35 @@ $detail = Yii::app()->getRequest()->getQuery('detail');
 
 $this->pageTitle=Yii::app()->name . ' | Kontrol Anggaran';
 ?>
-
-<div id="sidebar">
-	<?php $this->beginWidget('zii.widgets.CPortlet'); ?>
-	<ul>
-		<li><?php echo CHtml::link('Kontrol Anggaran Total', array('site/statistik', 'category'=>'1', 'chart'=>'1')) ?></li>
-		<li><?php echo CHtml::link('Kontrol Anggaran Divisi A', array('site/statistik', 'category'=>'2', 'chart'=>'1')) ?></li>
-		<li><?php echo CHtml::link('Kontrol Anggaran Divisi B', array('site/statistik', 'category'=>'2', 'chart'=>'1')) ?></li>
-	</ul>
-	<?php $this->endWidget(); ?>
-</div>
-
-<div id="maincontent">
-	<div id="menuform">
-		<h1 align="center" >Kontrol Anggaran</h1>
-		<?php $this->widget('zii.widgets.grid.CGridView', 
-			array( 'id'=>'anggaran-grid',
-			 'dataProvider'=>$dataanggaran,
-			 'columns'=>array(
-			  'Nama_Divisi',
-				// array(            // display using an expression
-				// 'name'=>'Nama Divisi',				
-				// 'value'=>'$data->nama_divisi', 
-				// 'filter'=>'',
-				// ),
-			  'pagu_anggaran',
-			  'nilai_rab',
-			  'nilai_hps',
-			 ),
-			));
-		?>
-	<!-----------
-		<table width="656" border="1" cellspacing="0" cellpadding="2">
-		<tr>
-		  <td width="200">Nama Divisi</td>
-		  <td width="100">Pagu Anggaran</td>
-		  <td width="100">Nilai RAB</td>
-		  <td width="100">Nilai HPS</td>
-		  <td width="100">Nilai Kontrak</td>
-		  <td width="100">Penghematan</td>
-		  <td width="100">Persen Penghematan</td>
-		</tr>
-		<?php
-		// foreach ($anggaran as $item) {
-		?>
-		<tr>
-		  <td><?php //echo $item[pagu_anggaran]; ?></td>
-		  <td><?php //echo $item[nilai_rab]; ?></td>
-		  <td><?php //echo $item[nilai_hps]; ?></td>
-		</tr>
-		<?php //} ?>
-		</table>
-	----->
-	</div>
-</div>
+<h1 align="center" >Kontrol Anggaran</h1>
+<p>
+	Pagu Total :  <?php echo $pagutotal; ?>
+	RAB Total :  <?php echo $rabtotal; ?> 
+	HPS Total :  <?php echo $hpstotal; ?>
+	Kontrak Total :  <?php echo $kontraktotal; ?>
+	Penghematan Total :  <?php echo $penghematantotal; ?>
+	Pesren Total :  <?php echo $persenpenghematantotal; ?> 
+</p>
+<?php $this->widget('zii.widgets.grid.CGridView', 
+	array( 'id'=>'anggaran-grid',
+		 'dataProvider'=>$dataanggaran,
+		 'htmlOptions'=>array('style'=>'cursor: pointer;'),			
+		 // 'selectionChanged'=>"function(id){window.location='" . Yii::app()->createUrl("anggaran/kontrolanggarandivisi", array("id"=>"$dataanggaran->getAttributeLabel(username)")) . "'+ $.fn.yiiGridView.getSelection(id);}",
+		 'columns'=>array(
+			'nama_divisi',
+			'pagu_anggaran',
+			'nilai_rab',
+			'nilai_hps',
+			'nilai_kontrak',
+			'penghematan',
+			'persen',
+			),
+		'pager'=>array(
+			'class'=>'CLinkPager',
+			'header'=>'',
+			'nextPageLabel'=>"Selanjutnya",
+			'prevPageLabel'=>'Sebelumnya',
+		),
+		'summaryText' => 'Keterangan : Angka dalam satuan rupiah.',
+	));
+?>
