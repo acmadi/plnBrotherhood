d44e733fb1d8f8141d320df017660a329f65e2d9
Irvan Aditya <iaditya9391@gmail.com>
2013-07-19 11:46:41 +0700

    numpang pull

---
+++ protected/views/admin/libur.php
@@ -0,0 +1,30 @@
+<?php
+	$this->pageTitle=Yii::app()->name . ' | Divisi';
+?>
+
+<div id="pagecontent">
+	<div id="sidebar">
+	<?php $this->beginWidget('zii.widgets.CPortlet'); ?>
+	<ul>
+		<li><?php echo CHtml::link('Tambah divisi', array('admin/tambahdivisi')) ?></li>
+		<li><?php echo CHtml::link('Hapus divisi', array('admin/hapusdivisi')) ?></li>
+	</ul>
+	<?php $this->endWidget(); ?>
+	</div>
+	<div id="maincontent">
+		<?php $this->widget('zii.widgets.grid.CGridView', array(
+			'dataProvider'=>$model->search(),
+			'htmlOptions'=>array('style'=>'cursor: pointer;'),			
+			'selectionChanged'=>"function(id){window.location='" . Yii::app()->createUrl("admin/detaildivisi", array("id"=>"$model->username")) . "'+ $.fn.yiiGridView.getSelection(id);}",
+			'columns'=>array(
+				array(
+					'name'=>'No',
+					'value'=>'$this->grid->dataProvider->pagination->currentPage * 10 + $row + 1',
+				),
+				'username',
+				'nama_divisi',
+			),
+		));
+		?>
+	</div>
+</div>
